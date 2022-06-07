27eb2faef7f2f27c64ebc9ddce7088dedb887276
step-be-step <zasweq41@gmail.com>
2022-06-07 11:11:55 +0300

    сойденение продаж не работает пустой значение

--- src/main/java/ru/bliz/sales/Sale.java
+++ src/main/java/ru/bliz/sales/Sale.java
@@ -2,7 +2,11 @@ package ru.bliz.sales;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.bliz.combocorm.Combicorm;
 import ru.bliz.concentrate.Concentrate;
+import ru.bliz.foodForCatAndDog.FoodForCatAndDog;
+import ru.bliz.grain_mix.Grain;
+import ru.bliz.other.OtherKorm;
 
 import javax.persistence.*;
 
@@ -20,4 +24,20 @@ public class Sale {
     @ManyToOne
     @JoinColumn(name = "concentrate_id")
     private Concentrate concentrate;
+
+    @ManyToOne
+    @JoinColumn(name = "other_korm_id")
+    private OtherKorm otherKorm;
+
+    @ManyToOne
+    @JoinColumn(name = "combicorm_id")
+    private Combicorm combicorm;
+
+    @ManyToOne
+    @JoinColumn(name = "grain_id")
+    private Grain grain;
+
+    @ManyToOne
+    @JoinColumn(name = "food_for_cat_and_dog_id")
+    private FoodForCatAndDog food;
 }
--- src/main/java/ru/bliz/sales/SaleController.java
+++ src/main/java/ru/bliz/sales/SaleController.java
@@ -6,8 +6,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import ru.bliz.combocorm.Combicorm;
+import ru.bliz.combocorm.CombicormRepo;
 import ru.bliz.concentrate.Concentrate;
 import ru.bliz.concentrate.ConcentrateRepo;
+import ru.bliz.foodForCatAndDog.FoodForCatAndDog;
+import ru.bliz.foodForCatAndDog.FoodForCatAndDogRepo;
+import ru.bliz.grain_mix.Grain;
+import ru.bliz.grain_mix.GrainRepo;
+import ru.bliz.other.OtherKorm;
+import ru.bliz.other.OtherKormRepo;
 
 import java.util.List;
 
@@ -19,13 +27,31 @@ public class SaleController {
 
     @Autowired
     private ConcentrateRepo concentrateRepo;
+    @Autowired
+    private OtherKormRepo otherKormRepo;
+    @Autowired
+    private CombicormRepo combicormRepo;
+    @Autowired
+    private GrainRepo grainRepo;
+    @Autowired
+    private FoodForCatAndDogRepo foodRepo;
 
     @GetMapping("/sales/new")
     public String showNewSaleFrom(Model model) {
         List<Concentrate> listConcentrates = concentrateRepo.findAll();
+        List<OtherKorm> listOther = otherKormRepo.findAll();
+        List<Combicorm> listCombicorms = combicormRepo.findAll();
+        List<FoodForCatAndDog> listFoods = foodRepo.findAll();
+        List<Grain> listGrain = grainRepo.findAll();
 
         model.addAttribute("sale", new Sale());
+
+        model.addAttribute("listOther", listOther);
         model.addAttribute("listConcentrates", listConcentrates);
+        model.addAttribute("listCombicorms", listCombicorms);
+        model.addAttribute("listFoods", listFoods);
+        model.addAttribute("listGrain", listGrain);
+
 
         return "sales_form";
     }
@@ -57,11 +83,20 @@ public class SaleController {
     @GetMapping("/sales/edit/{id}")
     public String showEditSaleForm(@PathVariable("id") Integer id, Model model) {
         Sale sale = saleRepo.findById(id).get();
+
+        List<OtherKorm> listOther = otherKormRepo.findAll();
+        List<Concentrate> listConcentrates = concentrateRepo.findAll();
+        List<Combicorm> listCombicorms = combicormRepo.findAll();
+        List<FoodForCatAndDog> listFoods = foodRepo.findAll();
+        List<Grain> listGrain = grainRepo.findAll();
+
         model.addAttribute("sale", sale);
 
-        List<Concentrate> listConcentrates = concentrateRepo.findAll();
-
+        model.addAttribute("listOther", listOther);
         model.addAttribute("listConcentrates", listConcentrates);
+        model.addAttribute("listCombicorms", listCombicorms);
+        model.addAttribute("listFoods", listFoods);
+        model.addAttribute("listGrain", listGrain);
 
         return "sales_form";
     }
